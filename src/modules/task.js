43ac37bd570108d368b71c426b4122d9ed7f7f7a
Kenneth Imade <kenneth.imade@yahoo.com>
2022-03-23 21:42:43 +0100

    setup task class

---
+++ src/modules/task.js
@@ -0,0 +1,12 @@
+
+class Task {
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.dueDate;
+    }
+
+    setDueDate(date) {
+        this.dueDate = date
+    }
+}
