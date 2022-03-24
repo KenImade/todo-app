0986d9566b9bf5934ef0c92e988c46f397dca3db
Kenneth Imade <kenneth.imade@yahoo.com>
2022-03-24 21:39:28 +0100

    added elements to sidebar

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,4 +1,7 @@
 import todoImg from '../assets/to-do.png'
+import inImg from '../assets/inbox.png'
+import tdImg from '../assets/calendar.png'
+import wkImg from '../assets/next-week.png'
 
 function createHeader() {
     const header = document.createElement('header')
@@ -18,11 +21,66 @@ function createHeader() {
     return header
 }
 
+function createBody() {
+    const appBody = document.createElement('section')
+    appBody.classList.add('app-body')
+    appBody.appendChild(createSideMenu())
+    appBody.appendChild(createTaskDisplay())
+    return appBody
+}
+
+function createSideMenu() {
+    const sideBar = document.createElement('div')
+    sideBar.classList.add('sidebar')
+
+    let inboxDiv = document.createElement('div')
+    inboxDiv.classList.add('sidebar-box')
+    let inboxText = document.createElement('p')
+    let inboxImg = document.createElement('img')
+    inboxImg.src = inImg
+    inboxImg.classList.add('sidebar-img')
+    inboxText.innerHTML = 'Inbox'
+    inboxDiv.appendChild(inboxImg)
+    inboxDiv.appendChild(inboxText)
+
+    let todayDiv = document.createElement('div')
+    let todayText = document.createElement('p')
+    todayDiv.classList.add('sidebar-box')
+    let todayImg = document.createElement('img')
+    todayImg.src = tdImg
+    todayImg.classList.add('sidebar-img')
+    todayText.innerHTML = 'Today'
+    todayDiv.appendChild(todayImg)
+    todayDiv.appendChild(todayText)
+
+    let weekDiv = document.createElement('div')
+    let weekText = document.createElement('p')
+    let weekImg = document.createElement('img')
+    weekImg.src = wkImg
+    weekDiv.classList.add('sidebar-box')
+    weekImg.classList.add('sidebar-img')
+    weekText.innerHTML = 'This week'
+    weekDiv.appendChild(weekImg)
+    weekDiv.appendChild(weekText)
+
+    sideBar.appendChild(inboxDiv)
+    sideBar.appendChild(todayDiv)
+    sideBar.appendChild(weekDiv)
+    return sideBar
+}
+
+function createTaskDisplay() {
+    const taskDisplay = document.createElement('div')
+    taskDisplay.classList.add('task-display')
+    return taskDisplay
+}
+
+
 
 function createUI() {
     const content = document.getElementById("container");
     content.appendChild(createHeader());
-
+    content.appendChild(createBody())
     return content
 }
 
