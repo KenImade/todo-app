bd7ab04b2f664af2eb22c1328752e6113a488648
Kenneth Imade <kenneth.imade@yahoo.com>
2022-03-25 16:39:53 +0100

    created task class

--- src/modules/task.js
+++ src/modules/task.js
@@ -1,12 +1,40 @@
-
-class Task {
+export default class Task {
     constructor(title, description) {
         this.title = title;
         this.description = description;
         this.dueDate;
+        this.completeStatus;
+    }
+
+    setTitle(title) {
+        this.title = title
+    }
+
+    setDescription(description) {
+        this.description = description
     }
 
     setDueDate(date) {
         this.dueDate = date
+    }
+
+    setCompleteStatus(completeStatus) {
+        this.completeStatus = completeStatus
+    }
+
+    getTitle() {
+        return this.title
+    }
+
+    getDescription() {
+        return this.description
+    }
+
+    getDueDate() {
+        return this.dueDate
+    }
+
+    getCompleteStatus() {
+        return this.completeStatus
     }
 }
