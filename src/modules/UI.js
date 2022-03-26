d5309ab84e5f6bf0d938719d47bdec0ffe0aaa11
Kenneth Imade <kenneth.imade@yahoo.com>
2022-03-26 15:37:53 +0100

    created reset page function

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -4,22 +4,15 @@ import Project from './project'
 
 export default class UI {
 
+    static content = document.getElementById('display')
+
+    static resetContentBox() {
+        this.content.innerHTML = ''
+    }
+
     static loadApp() {
         UI.loadInbox()
         UI.loadButtons()
-    }
-
-    static loadInbox() {
-        const content = document.getElementById('display')
-        const inboxPage = document.createElement('div')
-        const inboxTitle = document.createElement('h3')
-        inboxTitle.innerHTML = 'Inbox'
-        
-        inboxPage.appendChild(inboxTitle)
-
-        content.innerHTML = ''
-        content.appendChild(inboxPage)
-        return content
     }
 
     static loadButtons() {
@@ -38,30 +31,47 @@ export default class UI {
         let thisweekBtn = document.getElementById('week-btn')
         thisweekBtn.addEventListener('click', () => {
             console.log('week')
-            // this.loadThisWeeksTasks()
+            this.loadThisWeeksTasks()
+        })
+
+        let addProjectBtn = document.getElementById('add-project-btn')
+        addProjectBtn.addEventListener('click', () => {
+            console.log('adding project!!!')
         })
     }
 
+    static loadInbox() {
+        const inboxPage = document.createElement('div')
+        const inboxTitle = document.createElement('h3')
+        inboxTitle.innerHTML = 'Inbox'
+        
+        inboxPage.appendChild(inboxTitle)
+
+        UI.resetContentBox()
+        this.content.appendChild(inboxPage)
+        return this.content
+    }
+
     static loadTodayTasks() {
-        const content = document.getElementById('display')
         const todayPage = document.createElement('div')
         const todayTitle = document.createElement('h3')
         todayTitle.innerHTML = 'Today'
 
         todayPage.appendChild(todayTitle)
-        content.innerHTML = ''
-        content.appendChild(todayPage)
-        return content
+        UI.resetContentBox()
+        this.content.appendChild(todayPage)
+        return this.content
     }
 
     static loadThisWeeksTasks() {
-        const content = document.getElementById('display')
-        const inboxPage = document.createElement('div')
-        const inboxTitle = document.createElement('h3')
-        inboxTitle.innerHTML = 'This Week'
+        const thisweekPage = document.createElement('div')
+        const thisweekTitle = document.createElement('h3')
+        thisweekTitle.innerHTML = 'This Week'
 
-        todayPage.appendChild(todayTitle)
-        content.appendChild(todayPage)
-        return content
+        thisweekPage.appendChild(thisweekTitle)
+        UI.resetContentBox()
+
+        this.content.appendChild(thisweekPage)
+        return this.content
     }
 }
